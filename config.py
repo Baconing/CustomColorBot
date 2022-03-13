eb46fc5fa5b2bcb29cc8e938b139a00a5672530a
Bacon <57966530+Baconing@users.noreply.github.com>
2022-03-12 22:01:39 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 ####################
 #    Bot Config    #
 ####################
-OWNERS = [521808271368126467]    # Owner ID
-DEBUGPERMS = [521808271368126467]    # List of user IDs that can use debug commands
-PREFIX="!"   # Bot Prefix
+OWNERS = []    # Owner ID
+DEBUGPERMS = []    # List of user IDs that can use debug commands
+PREFIX=""   # Bot Prefix
 
 #######################
 #    Server Config    #
 #######################
-DEBUGSERVER = 910698852359757854    # Guild ID
+DEBUGSERVER =     # Guild ID
 
 #######################
 #   Database Config   #
 #######################
-CONNURL = "mongodb+srv://bot:X5dYLWg7xmfUx4jhRuREppZpqN4P52JMS4b6B8wG53R9nrSkbbsi@customcolorbot.8cuwk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CONNURL = "" # mongo db uri
